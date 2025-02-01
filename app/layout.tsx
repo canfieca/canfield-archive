e2fab03d622e4c0f4dbcb556b04f2ccf89895e67
canfieca <canfieca@oregonstate.edu>
2025-01-31 22:01:26 -0800

    designed the UI for the right side of navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,12 +28,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-hidden`}
       >
         <nav className="relative size-28 w-auto text-black bg-gray border-b-8 border-black flex items-center">
-          <ul className="absolute top-1/2 transform -translate-y-1/2 border border-black right-10 inline-flex px-10 space-x-6">
-            <li className="border border-black">Item 1</li>
-            <li>About</li>
-            <li>Inspiration</li>
-            <li>GitHub</li>
-            <li>Sign In</li>
+          <ul className="absolute top-1/2 transform -translate-y-1/2 right-10 inline-flex px-10 space-x-12 h-16 items-center">
+            <li className="text-2xl font-bold border-b-8 border-l-8 border border-black p-3 ">About</li>
+            <li className="text-2xl font-bold border-b-8 border-l-8 border border-black p-3 ">Inspiration</li>
+            <li className="text-2xl font-bold border-b-8 border-l-8 border border-black p-3 ">GitHub</li>
+            <li className="text-2xl font-bold p-3 pr-7 pl-7 border border-black rounded-full bg-gray-800 text-white">Sign In</li>
           </ul>
         </nav>
         {children}
