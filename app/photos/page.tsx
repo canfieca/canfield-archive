aad0e647f9d1259fc69d8998e2adc2d30eb0d174
canfieca <canfieca@oregonstate.edu>
2025-04-17 17:14:50 -0700

    Implemented bootstrap grid system. Much better!

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,43 +1,10 @@
-"use client";
-import '../../css/globals.css';
-import PhotoCard from '@/app/ui/photos/photoCard';
-import { useEffect, useState } from 'react';
-
-interface Photo {
-    '0th'?: {
-        ImageDescription?: string;
-    };
-    // Add other properties if needed
-}
+import PhotoGallery from "@/app/ui/photos/PhotoGallery";
 
 export default function Photos() {
-    const [photos, setPhotos] = useState<Record<string, Photo>>({});
-
-    useEffect(() => {
-        async function fetchPhotos() {
-            const response = await fetch('/exif_data.json');
-            const data = await response.json();
-            setPhotos(data);
-        }
-
-        fetchPhotos();
-    }, []);
-
-    const photoCards = Object.keys(photos).map((key, index) => {
-        const photo = photos[key];
-        const src = `/Test Photos/${key}`; // Adjust the path as needed
-        const title = photo['0th']?.ImageDescription || key;
-
-        return <PhotoCard 
-                    key={index} 
-                    src={src} 
-                    title={title} 
-                />;
-    });
-
     return (
-        <div className="m-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {photoCards}
+        <div>
+            <h1 className="text-center mt-4">Photo Gallery</h1>
+            <PhotoGallery />
         </div>
-    );
+    )
 }
