5614b49d580f4ee132eb102857af2a2503dc4957
canfieca <canfieca@oregonstate.edu>
2025-04-21 18:11:00 -0700

    Fixed SideNav component

--- app/photos/layout.tsx
+++ app/photos/layout.tsx
@@ -1,15 +1,19 @@
 import "../../css/globals.css";
 import SideNav from "@/app/ui/photos/sideNav";
+import { getYears } from "@/app/lib/getYears";
 
 
-export default function PhotoLayout({
+export default async function PhotoLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const years = await getYears(); // server-side fetch
+
     return (
         <div className="flex min-h-screen">
-            <SideNav />
+            <SideNav years={years} />
             <main className="flex-1 antialiased overflow-auto no-scrollbar">
                 {children}
             </main>
--- app/ui/photos/sideNav.tsx
+++ app/ui/photos/sideNav.tsx
@@ -1,19 +1,16 @@
-export default function SideNav() {
+import "@/css/globals.css";
 
-    const years = ['Undefined', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025']
-
+export default function SideNav({ years }: {years: string[] }) {
     return (
-        <>
-            <nav className="border border-black w-[20%] h-[100vh] text-black">
-                <ul className="p-0">
-                    {years.map((year) => (
-                        <li key={year}>
-                            {year}
-                            <hr className="border-black" />
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </>
+        <nav className="border border-black w-[20%] h-[100vh] text-black overflow-scroll no-scrollbar">
+            <ul className="p-0 h-full mt-2">
+                {years.map((year) => (
+                    <li key={year} className="text-lg font-bold text-center">
+                        {year}
+                        <hr className="border-black" />
+                    </li>
+                ))}
+            </ul>
+        </nav>
     );
 }
