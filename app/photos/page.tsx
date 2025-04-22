6cee18420c733900db4e77c98f27d4fdf4ca07e8
canfieca <canfieca@oregonstate.edu>
2025-04-21 18:14:55 -0700

    Removed Photo Gallery text

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -3,7 +3,6 @@ import PhotoGallery from "@/app/ui/photos/PhotoGallery";
 export default function Photos() {
     return (
         <div>
-            <h1 className="text-center mt-4">Photo Gallery</h1>
             <PhotoGallery />
         </div>
     )
