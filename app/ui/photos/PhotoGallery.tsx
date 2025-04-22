3a9062950b78bbb56f4c8daf9083c582383fa16f
canfieca <canfieca@oregonstate.edu>
2025-04-21 22:17:26 -0700

    Deleted hard coded photos array

--- app/ui/photos/PhotoGallery.tsx
+++ app/ui/photos/PhotoGallery.tsx
@@ -3,28 +3,6 @@ import Image from "next/image";
 import Link from "next/link";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// const photos = [
-//     '/Test Photos/40th Party & Vermont 0043.jpg',
-//     '/Test Photos/2006 0001.jpg',
-//     '/Test Photos/Brooklyn Blockparty 2002 & John\'s Birthday 0095.jpg',
-//     '/Test Photos/Brooklyn Blockparty 2002 & John\'s Birthday 0098.jpg',
-//     '/Test Photos/Cruise Nov 2008 0005.jpg',
-//     '/Test Photos/Dec 26 1996 0016.jpg',
-//     '/Test Photos/Disney & Florida 2008 0018.jpg',
-//     '/Test Photos/Disney 1999 0080.jpg',
-//     '/Test Photos/Disney World 0020.jpg',
-//     '/Test Photos/IMG_0142.jpg',
-//     '/Test Photos/IMG_0259 (2).jpg',
-//     '/Test Photos/IMG_0323.jpg',
-//     '/Test Photos/IMG_0473 (1).jpg',
-//     '/Test Photos/IMG_0529.jpg',
-//     '/Test Photos/IMG_0685.jpg',
-//     '/Test Photos/IMG_0737 (1).jpg',
-//     '/Test Photos/IMG_1150 (4).jpg',
-//     '/Test Photos/IMG_1347 (1).jpg',
-//     '/Test Photos/DJI_0018 (2).jpg'
-// ];
-
 interface Props {
     photos: { key: string; url: string }[];
     year: string;
