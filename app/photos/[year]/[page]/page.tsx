e66632fcf25fe7e00855f82c88671f4d3ce62bce
canfieca <canfieca@oregonstate.edu>
2025-04-24 16:37:49 -0700

    Fixed the params prop

--- app/photos/[year]/[page]/page.tsx
+++ app/photos/[year]/[page]/page.tsx
@@ -2,11 +2,11 @@ import PhotoGallery from "@/app/ui/photos/PhotoGallery";
 import { getPhotosByYear } from "@/app/lib/getPhotosByYear";
 
 interface Props {
-    params: { year: string, page: string }
+    params: Promise<{ year: string, page: string }>;
 }
 
 export default async function YearPage({ params }: Props) {
-    const { year, page } = params;
+    const { year, page } = await params;
     const pageNum = parseInt(page, 10) || 1;
     const pageSize = 42;
 
