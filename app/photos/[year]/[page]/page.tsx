bebde357ec76e0c02d9de385e75e2990af810430
canfieca <canfieca@oregonstate.edu>
2025-04-24 19:28:57 -0700

    Updated it to match Next.js 15

--- app/photos/[year]/[page]/page.tsx
+++ app/photos/[year]/[page]/page.tsx
@@ -1,12 +1,14 @@
 import PhotoGallery from "@/app/ui/photos/PhotoGallery";
 import { getPhotosByYear } from "@/app/lib/getPhotosByYear";
 
+type Params = Promise<{ year: string; page: string }>
+
 export default async function YearPage({
     params,
 }: {
-    params: { year: string; page: string };
+    params: Params
 }) {
-    const { year, page } = params;
+    const { year, page } = await params;
     const pageNum = parseInt(page, 10) || 1;
     const pageSize = 42;
 
